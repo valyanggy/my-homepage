fb9a53b123d6061e11b1589fe3f52555279845fc
Val Yang <yangx927@newschool.edu>
2021-09-12 16:32:32 -0400

    basic function(not finished)
    needs to fix the toggle function

---
+++ js/index.js
@@ -0,0 +1,9 @@
+// toggle to show listing
+function switchContent() {
+    var x = document.querySelector('.portal');
+    if (x.style.display === "none") {
+        x.style.disply = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
