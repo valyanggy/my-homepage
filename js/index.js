ec2842dfd0567bd726ffb11f0619952391cc1856
Val Yang <yangx927@newschool.edu>
2021-09-15 21:25:00 -0400

    add some minor animation and fix the toggle
    fixed by adding jquery

--- js/index.js
+++ js/index.js
@@ -1,9 +1,20 @@
-// toggle to show listing
-function switchContent() {
-    var x = document.querySelector('.portal');
-    if (x.style.display === "none") {
-        x.style.disply = "block";
-    } else {
-        x.style.display = "none";
-    }
-}
+// toggle to show listing from https://codepen.io/rinatoptimus/pen/GjrWNd
+$(document).ready(function() {
+    $('.nav-toggle').click(function() {
+        //get collapse content selector
+        var collapse_content_selector = $(this).attr('href');
+
+        //make the collapse content to be shown or hide
+        var toggle_switch = $(this);
+        $(collapse_content_selector).toggle(function() {
+            if ($(this).css('display') == 'none') {
+                //change the button label to be 'Show'
+                toggle_switch.html('oxueyo with the links');
+            } else {
+                //change the button label to be 'Hide'
+                toggle_switch.html('oxueyo');
+            }
+        });
+    });
+
+});
